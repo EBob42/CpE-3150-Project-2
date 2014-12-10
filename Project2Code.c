44690aa12f0986c61a6f6426524102b44cfb7d9b
djk6x2 <djk6x2@mst.edu>
2014-12-09 22:23:20 -0600

    Update Project2Code.c

--- Project2Code.c
+++ Project2Code.c
@@ -1,12 +1,5 @@
 #include "project2.h"
 
-void test() interrupt 2
-{
-  transmit("interrupt", 9);
-  return;
-}
-
-
 //TODO: Come up with the extra stuff we want to add
 //			(each person decide stuff, we can probably
 //			work together to make it happen).
@@ -15,13 +8,12 @@ void main()
   P2M1 = 0;
   P1M1 = 0;
   P0M1 = 0;
-
-	IEN0 = 0x88;
+  clearSevenSeg();
+  
+  IEN0 = 0x88;
 	
   transmit("song 1", 6);
   transmit("song 2", 6);
-
-  //playSong(STAR);
 
   while(1)
   {
